81da56ad3495dafdfe0a06cbdd6a1eaa2a2b0778
eshinfg <eshinx1@gmail.com>
2019-03-24 20:35:54 -0700

    [Eric] Breath First and Depth first traversal :

--- BST.js
+++ BST.js
@@ -15,22 +15,22 @@ class BST {
 
   insert(val) {
     let newNode = new Node(val);
-    if(this.root === null) {
+    if (this.root === null) {
       this.root = newNode;
       return;
     }
     let current = this.root;
-    while(true) {
-      if(val === current.val) return undefined;
+    while (true) {
+      if (val === current.val) return undefined;
 
-      if(val > current.val) {
-        if(current.right === null) {
+      if (val > current.val) {
+        if (current.right === null) {
           current.right = newNode;
           return this;
         }
         current = current.right;
       } else {
-        if(current.left === null) {
+        if (current.left === null) {
           current.left = newNode;
           return this;
         }
@@ -40,33 +40,33 @@ class BST {
   }
 
   contains(val) {
-    if(this.root.val === null) return false;
+    if (this.root.val === null) return false;
     let current = this.root;
     let found = false;
 
-    while(current && !found) {
-      if(val < current.value) {
+    while (current && !found) {
+      if (val < current.value) {
         current = current.left;
-      } else if(value > current.value) {
-        current = current.right
+      } else if (value > current.value) {
+        current = current.right;
       } else {
         return true;
       }
     }
     return false;
-  } 
+  }
 
   contains(val) {
-    if(this.root.val === null) return false;
+    if (this.root.val === null) return false;
     let current = this.root;
     let found = false;
 
     while (current && !found) {
-      if(val < current.value) {
-        current = current.left;  
+      if (val < current.value) {
+        current = current.left;
       } else if (val > current.value) {
         current = current.right;
-      } else { 
+      } else {
         return true;
       }
     }
@@ -74,11 +74,11 @@ class BST {
   }
 
   find() {
-    if(this.root === null) return false;
+    if (this.root === null) return false;
     let current = this.root;
     let found = false;
     while (current && !found) {
-      if(value < current.value) {
+      if (value < current.value) {
         current = current.left;
       } else if (value > current.value) {
         current = current.right;
@@ -89,22 +89,97 @@ class BST {
     if (!found) return undefined;
     return current;
   }
+  //________________
+  // Breath First Search
+  // Trying to visit Every Node
+  //    10
+  //    / \
+  //   6   15
+  //  /\    /\
+  // 1  8  12 18
 
+  // return [10,6,15,1,8,12,18]
   breathFirstSearch() {
     let node = this.root;
     let data = [];
     let queue = [];
     queue.push(node);
 
-    while(queue.length) {
+    while (queue.length) {
       // take node out of queue
       node = queue.shift();
-      // push that node into data;
+      // push that node value data;
       data.push(node.value);
-      if(node.left) queue.push(node.left);
-      if(node.right) queue.push(node.right);
+      // add left side to queue;
+      if (node.left) queue.push(node.left);
+      // add right side to queue;
+      if (node.right) queue.push(node.right);
     }
     return data;
   }
 
+  // _______________
+  // DepthFirstSearch - Pre Order
+  //entire left side than right side
+  //    10
+  //    / \
+  //   6   15
+  //  /\    /\
+  // 1  8  12 18
+
+  // [10,6,1,8,15,12,18]
+  depthFirstPreOrder() {
+    let data = [];
+    let traverse = node => {
+      data.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return data;
+  }
+
+  
+  // _______________
+  // DepthFirstSearch - Post Order
+  // Left Bottom node than right bottom node up to root
+  // Root is last to visit
+  //    10
+  //    / \
+  //   6   15
+  //  /\    /\
+  // 1  8  12 18
+  // [1,8,6,12,18,15,10]
+
+  depthFirstInOrder() {
+    let data = [];
+    let traverse = node => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.val);
+    };
+    traverse(this.root);
+    return data;
+  }
+
+
+  // _______________
+  // DepthFirstSearch - In Order
+  //entire left side than right side
+  //    10
+  //    / \
+  //   6   15
+  //  /\    /\
+  // 1  8  12 18
+  // [1,6,8,10,12,15,18]
+  depthFirstInOrder() {
+    let data = [];
+    let traverse = node => {
+      if (node.left) traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return data;
+  }
 }
